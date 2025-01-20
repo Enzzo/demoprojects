b1a308cd8bf62a546c674277b90aee6f6939ea18
unknown <enzzomatrix@gmail.com>
2025-01-21 08:44:12 +1100

    simple auth project init

---
+++ 02/simpleauthexample/src/main/java/ru/vasilev/simpleauthexample/SimpleauthexampleApplication.java
@@ -0,0 +1,13 @@
+package ru.vasilev.simpleauthexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleauthexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleauthexampleApplication.class, args);
+	}
+
+}
---
+++ 02/simpleauthexample/src/test/java/ru/vasilev/simpleauthexample/SimpleauthexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vasilev.simpleauthexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleauthexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
